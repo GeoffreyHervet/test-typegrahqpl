c39eb80f68421ef5b3cfe04012c3fa270a955996
Geoffrey Hervet <hervetg@gmail.com>
2020-07-30 23:38:50 +0200

    POC Working

--- src/fieldResolver.ts
+++ src/fieldResolver.ts
@@ -1,11 +1,27 @@
-import { Resolver, ResolverInterface, FieldResolver, Root, Ctx } from 'type-graphql';
+import { Resolver, ResolverInterface, FieldResolver, Root, Ctx, Info } from 'type-graphql';
 import { Foo, Category, GraphQlContext } from './types';
+import { GraphQLResolveInfo } from 'graphql';
 
 @Resolver(() => Category)
 export default class FooResolver implements ResolverInterface<Category> {
   @FieldResolver()
-  async foo(@Root() category: Category, @Ctx() context: GraphQlContext): Promise<Foo | undefined> {
+  async foo(
+    @Root() category: Category,
+    @Ctx() context: GraphQlContext,
+    @Info() info: GraphQLResolveInfo,
+  ): Promise<Foo | undefined> {
     console.log({ context });
+    console.log({ info });
+    console.log(info.fieldNodes);
+    const location = info.fieldNodes[0].selectionSet?.loc;
+    const {
+      start,
+      end,
+      source: { body },
+    } = location!;
+    const selectedData = body.slice(start, end);
+    console.log(selectedData);
+
     return {
       a: 'A',
       b: 'b',
